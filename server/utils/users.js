3894071b503152db2a0962b934603aa13f62586f
Daniels4624P <danielfelipesaenzbaquero@gmail.com>
2025-06-18 20:43:37 -0500

    Fix lastTimeOnlinne

--- server/utils/users.js
+++ server/utils/users.js
@@ -8,6 +8,16 @@ export const getConnectedUsers = async () => {
     userID: user.userID,
     username: user.username,
     connected: user.connected,
+    unreadCount: 0
+  }));
+};
+
+export const getDisconnectedUsers = async () => {
+  const users = await User.find({ connected: false }).select("userID username connected lastTimeOnline");
+  return users.map((user) => ({
+    userID: user.userID,
+    username: user.username,
+    connected: user.connected,
     unreadCount: 0,
     lastTimeOnline: user.lastTimeOnline
   }));
